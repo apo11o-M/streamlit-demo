45ac1cb74450747be3dee29164b32f3de7ac7c37
Rick Wang <54286072+apo11o-M@users.noreply.github.com>
2025-06-22 00:09:42 +0000

    lesson 5 commit
    removed api key

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+from openai import OpenAI
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Hi there!")
+
+client = OpenAI(
+    api_key = ""
 )
+
+chat_history = [
+    # {"role": "system", "content": "You are a helpful assistant, you can only respond in chinese"},
+    {"role": "system", "content": "Please translate all user input into Chinese."}
+]
+
+user_input = st.text_input("Enter your message: ")
+
+chat_history.append({"role": "user", "content": user_input})
+
+# sending a request to OpenAI through the OpenAI API
+response = client.chat.completions.create(
+    model="gpt-4.1",
+    # model="gpt-3.5-turbo-0125",
+    messages=chat_history,
+)
+
+# print the response
+st.write(response.choices[0].message.content)
+chat_history.append({"role": "assistant", "content": response.choices[0].message.content})
