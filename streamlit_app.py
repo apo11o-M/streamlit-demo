0e61960dbc3e0c0b6b472fd8f0a53ed7649d3341
Rick Wang <54286072+apo11o-M@users.noreply.github.com>
2025-06-28 23:48:00 +0000

    added streamlit session for chat history

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,22 +11,30 @@ client = OpenAI(
     api_key = open_ai_api_key
 )
 
-chat_history = [
-    # {"role": "system", "content": "You are a helpful assistant, you can only respond in chinese"},
-    {"role": "system", "content": "Please translate all user input into Chinese."}
-]
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = [
+        {"role": "system", "content": "Please translate all user input into Chinese."}
+    ]
 
 user_input = st.text_input("Enter your message: ")
 
-chat_history.append({"role": "user", "content": user_input})
+st.session_state["chat_history"].append({"role": "user", "content": user_input})
 
 # sending a request to OpenAI through the OpenAI API
 response = client.chat.completions.create(
     model="gpt-4.1",
     # model="gpt-3.5-turbo-0125",
-    messages=chat_history,
+    messages=st.session_state["chat_history"],
 )
 
 # print the response
 st.write(response.choices[0].message.content)
-chat_history.append({"role": "assistant", "content": response.choices[0].message.content})
+st.session_state["chat_history"].append({"role": "assistant", "content": response.choices[0].message.content})
+
+
+# Question:
+# What are some use cases for the "Session" feature for the application above?
+# You have a couple minutes to think about this.
+
+# Practice:
+# Modify the 'chat_history' variable so the chat_history list is stored in the streamlit.session_state
